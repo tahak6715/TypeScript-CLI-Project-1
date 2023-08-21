4ed1b6316ff5f07c50d34361b0cf7cd6825c07a4
Taha khan <tahak6715@gmail.com>
2023-08-21 09:29:53 -0700

    work complete

---
+++ app.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgBlueBright("Welcome to the CLI Calculator.."))
+
+let answers = await inquirer.prompt([{
+    name: "num1",
+    type: "number",
+    message: "Enter First Number:"}
+]);
+
+let answers1 = await inquirer.prompt([{
+    name: "num2",
+    type: "number",
+    message: "Enter Secong Number:"}
+]);
+
+let operation = await inquirer.prompt([{
+    name: "oop",
+    type: "string",
+    message: "Enter operation: + - x /"}
+]);
+
+if(operation.oop === "x"){
+    const res : number = answers.num1 * answers1.num2; 
+    console.log(chalk.blue(`The Answer is ${res}`));
+}
+else if(operation.oop === "+"){
+    const res : number = answers.num1 + answers1.num2; 
+    console.log(chalk.blue(`The Answer is ${res}`));
+}
+else if(operation.oop === "-"){
+    const res : number = answers.num1 - answers1.num2; 
+    console.log(chalk.blue(`The Answer is ${res}`));
+}
+else if(operation.oop === "/"){
+    const res : number = answers.num1 / answers1.num2; 
+    console.log(chalk.blue(`The Answer is ${res}`));
+}
+else{
+    console.log(chalk.blue(`Invalid Operation...`));
+}
+
+
+
+// console.log(chalk.blue("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old."));
+
